4bfeb1f9dd7b201a2b1db2a8b9022c227e965e6c
raf98 <rafaelrutzsantos@gmail.com>
2024-09-25 06:49:09 -0300

    Removed synchronized from GlobalClock, which caused all clients to use the game global clock, seemingly, but the currentTime update now can be either inconsistent or the same as before, where each client still updates its currentTime local variable, instead of the global one, as it should. Working with a Lamport-ish manner to update the global clock also did not work, when using different methods to get the currentTime and update it.

--- TinyTM/GlobalClock.java
+++ TinyTM/GlobalClock.java
@@ -12,8 +12,7 @@ public class GlobalClock implements IGlobalClock {
         System.out.println(currentTime.get());
     }
 
-    @Override
-    public synchronized long getCurrentTime() throws RemoteException, Exception {
+    public long getCurrentTime() throws RemoteException, Exception {
         //System.out.println("GLOBAL CLOCK OBJ: " + this.hashCode());
         //System.out.println("CURRENT TIME: " + currentTime.get());
         return currentTime.incrementAndGet();//System.currentTimeMillis(); //System.nanoTime();
