45c7df20f24b91523c8fabd7896ec7ee24fd3b07
raf98 <rafaelrutzsantos@gmail.com>
2025-01-23 23:03:28 -0300

    Added commented code for a possible alternative to Agressive's conflict handling, in order to prevent livelock loops.

--- TinyTM/contention/Aggressive.java
+++ TinyTM/contention/Aggressive.java
@@ -9,7 +9,24 @@ public class Aggressive extends ContentionManager {
 
     @Override
     public void resolve(Transaction me, ITransaction other) throws RemoteException {
-        other.abort();
+        // if the current attacking transaction is conflicting with an enemy that aborted it previosly, then
+        // it should be aborted again, to enforce the agressive policy from the previous transaction abort and, thus,
+        // prevent a livelock loop with it
+        // it should also check if the enemy is defunct and, if so, abort it
+        // this could be done by a max attempts parameter, that controls the number of times 
+        // an enemy attempted to complete itself after conflicting and, if it passes this limit,
+        // the enemy is set to defunct, so that it can be aborted, since both its attempts and
+        // defunct flag would be reset whenever it opens a new transactional object
+       // if (!other.getConflictList().contains(me.hashCode()) || other.getDefunct()) {
+            other.abort();
+        //    me.getConflictList().add(other.hashCode());
+        //} else {
+            /*other.setAttempts(other.getAttempts() + 1);
+            if (other.getAttempts() > attempts) {
+                other.setDefunct(true);
+            }*/
+            //me.abort();
+        //}
     }
 
     @Override
