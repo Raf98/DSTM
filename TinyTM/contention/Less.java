2d2f224194cea22cbe9f411ba8d5a6142b7bdb9a
raf98 <rafaelrutzsantos@gmail.com>
2025-01-28 03:54:34 -0300

    Updated less to use the actual aborts of a transaction, not the overall aborts from a machine.

--- TinyTM/contention/Less.java
+++ TinyTM/contention/Less.java
@@ -9,7 +9,7 @@ import TinyTM.exceptions.AbortedException;
 public class Less extends ContentionManager {
     public void resolve(Transaction me, ITransaction other) throws RemoteException {
 
-        if (me.getAborts() <= other.getAborts()) {
+        if (me.getTransactionAborts() <= other.getTransactionAborts()) {
             other.abort();
         } else {
             me.abort();
